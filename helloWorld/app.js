9e82173a270013fb8f530d62f0b77fd4c1567251
root <root@linux.(none)>
2017-05-27 03:19:14 -0700

    函数的返回值

--- helloWorld/app.js
+++ helloWorld/app.js
@@ -1,18 +1,14 @@
 console.log("the app start");
 
-var a=3;              //赋值
-a=5;                  // 覆盖原有值，数值类型
-a='hello world';     //重新赋值，字符串类型
-
-
-var b = 3 + 5;
-
-var str = a + ' hehe';   //字符串相加
-
-console.log(str);
-
-function f(v) {
-  console.log(v);
+function a() {
+  return 100*10;
 }
 
-f('a=' + a + ';  b=' + b + ' ; str=' +str);
+function f() {
+  return a;
+}
+
+
+
+console.log(f());
+
