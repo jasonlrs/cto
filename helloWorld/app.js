2f169dac9f85d286da253ca4945a19bf07b1c258
root <root@linux.(none)>
2017-05-27 03:27:58 -0700

    匿名函数

--- helloWorld/app.js
+++ helloWorld/app.js
@@ -1,16 +1,8 @@
 console.log("the app start");
 
-function a() {
-  return 100*10;
-}
+// 匿名函数，声明一个函数时，用圆括号包住该函数，使用()();格式可执行匿名函数
+//
+(function() {
+  console.log('这是一个匿名函数');
+})();
 
-function f() {
-  return a;              // 不带括号，表示这个函数对象
-  // return a();        //带括号表示，要执行函数
-}
-
-
-
-console.log(f());
-
-console.log((f())());   // 用小括号包住函数对象，后面再跟一个小括号，表示执行该函数
