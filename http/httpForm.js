c895db12a300830718d96547ce31e609dd5df382
root <root@linux.(none)>
2017-05-28 00:52:22 -0700

    post请求

--- http/httpForm.js
+++ http/httpForm.js
@@ -1,5 +1,11 @@
 var http = require('http');
 
+//http服务器监听浏览器的请求
+// 当接收的是GET方式请求req，使用doGet(req,res)处理请求
+// 当接收的是POST方式请求req,使用doPost(req,res)处理请求
+//
+// req, 是浏览器发过来的请求（对象 ）
+// res，是服务器向客户端的发送的回复信息对象
 http.createServer(function(req, res) {
   // html有协议头Head，和协议体Body
   // 协议头'text/plain'表示回复的信息格式为文本块； 'text/html'表示回复的信息格式为html
